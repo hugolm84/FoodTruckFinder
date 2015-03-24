04d85ae2064ec5d28f073860ab8979f833a153a0
Hugo Lindström <hugolm84@gmail.com>
2015-03-24 12:06:44 +0100

    Shared preferences

--- app/src/main/java/pb/foodtruckfinder/Activity/SettingsPreferenceActivity.java
+++ app/src/main/java/pb/foodtruckfinder/Activity/SettingsPreferenceActivity.java
@@ -2,6 +2,50 @@ package pb.foodtruckfinder.Activity;
 
 /**
  * Created by hugo on 24/03/15.
+ *//**
+ * Hugo Lindström (C) 2013
+ * huli1000
  */
-public class SettingsPreferenceActivity {
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+import pb.foodtruckfinder.Fragment.SettingsFragment;
+import pb.foodtruckfinder.R;
+
+
+public class SettingsPreferenceActivity extends Activity implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    SharedPreferences preferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(getResources().getString(R.string.action_settings));
+
+        // Display the fragment as the main content.
+        if (savedInstanceState == null)
+            getFragmentManager().beginTransaction().add(android.R.id.content, new SettingsFragment()).commit();
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        preferences.registerOnSharedPreferenceChangeListener(this);
+
+    }
+
+    // Updates the fragment/view
+    protected void updateFragment() {
+        getFragmentManager().beginTransaction().replace(android.R.id.content,
+                new SettingsFragment()).commit();
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        updateFragment();
+    }
+
+    public String getPreferenceString(Integer id, Integer defaultId) {
+        return preferences.getString(getResources().getString(id), getResources().getString(defaultId));
+    }
 }
--- app/src/main/java/pb/foodtruckfinder/Fragment/SettingsFragment.java
+++ app/src/main/java/pb/foodtruckfinder/Fragment/SettingsFragment.java
@@ -1,7 +1,24 @@
 package pb.foodtruckfinder.Fragment;
 
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import pb.foodtruckfinder.R;
+
 /**
  * Created by hugo on 24/03/15.
  */
-public class SettingsFragment {
+public class SettingsFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public String toString() {
+        return "Inställningar";
+    }
 }
