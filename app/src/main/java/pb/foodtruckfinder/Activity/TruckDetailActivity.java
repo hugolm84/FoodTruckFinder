ceed1f87a0e9b65400c178e59df83e38746fd8b9
Hugo Lindström <hugolm84@gmail.com>
2015-03-24 12:07:46 +0100

    Remove Fab transitions, add menuOptions

--- app/src/main/java/pb/foodtruckfinder/Activity/TruckDetailActivity.java
+++ app/src/main/java/pb/foodtruckfinder/Activity/TruckDetailActivity.java
@@ -1,30 +1,24 @@
 package pb.foodtruckfinder.Activity;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import pb.foodtruckfinder.BaseActivity;
 import pb.foodtruckfinder.Fragment.MapFragment;
-import pb.foodtruckfinder.Fragment.PlaceholderFragment;
 import pb.foodtruckfinder.Fragment.ReviewFragment;
 import pb.foodtruckfinder.Fragment.TruckProfileFragment;
 import pb.foodtruckfinder.Fragment.ViewPagerTab2GridViewFragment;
 import pb.foodtruckfinder.Fragment.ViewPagerTab2ListViewFragment;
 import pb.foodtruckfinder.R;
 
-import android.annotation.TargetApi;
-import android.content.res.Configuration;
-import android.os.Build;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
@@ -40,7 +34,6 @@ import com.github.ksoichiro.android.observablescrollview.Scrollable;
 import com.github.ksoichiro.android.observablescrollview.TouchInterceptionFrameLayout;
 import com.google.samples.apps.iosched.ui.widget.SlidingTabLayout;
 import com.nineoldandroids.view.ViewHelper;
-import com.nineoldandroids.view.ViewPropertyAnimator;
 
 /**
  * This uses TouchInterceptionFrameLayout to move Fragments.
@@ -57,16 +50,13 @@ public class TruckDetailActivity extends BaseActivity implements ObservableScrol
     private TextView mTitleView;
 
     private View mFab;
-    private int mFabMargin;
-    private boolean mFabIsShown;
-    private int mFlexibleSpaceShowFabOffset;
-    private int mFlexibleSpaceImageHeight;
     private TouchInterceptionFrameLayout mInterceptionLayout;
     private ViewPager mPager;
     private NavigationAdapter mPagerAdapter;
     private int mSlop;
     private int mFlexibleSpaceHeight;
     private int mTabHeight;
+    private int mFabMargin;
     private boolean mScrolled;
     private MapFragment mMapFragment;
 
@@ -79,7 +69,6 @@ public class TruckDetailActivity extends BaseActivity implements ObservableScrol
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
         Intent intent = getIntent();
         String message = intent.getStringExtra("TRUCK_TITLE");
 
@@ -91,11 +80,7 @@ public class TruckDetailActivity extends BaseActivity implements ObservableScrol
         mPager.setAdapter(mPagerAdapter);
         mImageView = findViewById(R.id.image);
         mOverlayView = findViewById(R.id.overlay);
-        // Padding for ViewPager must be set outside the ViewPager itself
-        // because with padding, EdgeEffect of ViewPager become strange.
 
-
-        mFlexibleSpaceShowFabOffset = getResources().getDimensionPixelSize(R.dimen.flexible_space_show_fab_offset);
         mFlexibleSpaceHeight = getResources().getDimensionPixelSize(R.dimen.flexible_space_image_height);
         mTabHeight = getResources().getDimensionPixelSize(R.dimen.tab_height);
 
@@ -112,21 +97,13 @@ public class TruckDetailActivity extends BaseActivity implements ObservableScrol
                 Toast.makeText(TruckDetailActivity.this, "FAB is clicked", Toast.LENGTH_SHORT).show();
             }
         });
-/*        mFabMargin = getResources().getDimensionPixelSize(R.dimen.margin_standard);
 
-        int maxFabTranslationY = mFlexibleSpaceHeight - mTabHeight - mFab.getHeight() / 2;
-        ViewHelper.setTranslationX(mFab, mFabMargin);
-        ViewHelper.setTranslationY(mFab, maxFabTranslationY);*/
-
-        /*ViewHelper.setTranslationX(mFab, mFabMargin);
-        ViewHelper.setTranslationY(mFab, -mFlexibleSpaceImageHeight);
-        */
 
 
         SlidingTabLayout slidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
         slidingTabLayout.setCustomTabView(R.layout.tab_indicator, android.R.id.text1);
         slidingTabLayout.setSelectedIndicatorColors(getResources().getColor(R.color.accent));
-        slidingTabLayout.setDistributeEvenly(false);
+        slidingTabLayout.setDistributeEvenly(true);
 
         slidingTabLayout.setViewPager(mPager);
         ((FrameLayout.LayoutParams) slidingTabLayout.getLayoutParams()).topMargin = mFlexibleSpaceHeight - mTabHeight - mTitleView.getHeight();
@@ -148,25 +125,6 @@ public class TruckDetailActivity extends BaseActivity implements ObservableScrol
 
     @Override
     public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
-
-
-        // Translate FAB
-       /* int maxFabTranslationY = mFlexibleSpaceHeight - mTabHeight - mFab.getHeight() / 2;
-        float fabTranslationY = ScrollUtils.getFloat(
-                -scrollY + mFlexibleSpaceHeight - mFab.getHeight(),
-                mTabHeight - mFab.getHeight(),
-                maxFabTranslationY);
-
-        ViewHelper.setTranslationX(mFab, mFabMargin);
-        ViewHelper.setTranslationY(mFab, fabTranslationY);
-*/
-        /*Log.d("TruckDetail", "fab:" + fabTranslationY + " offset:" + mFlexibleSpaceHeight);
-        // Show/hide FAB
-        if (fabTranslationY < mFlexibleSpaceShowFabOffset) {
-            hideFab();
-        } else {
-            showFab();
-        }*/
 
     }
 
@@ -200,10 +158,6 @@ public class TruckDetailActivity extends BaseActivity implements ObservableScrol
             boolean scrollingDown = diffY < 0;
             if (scrollingUp) {
                 if (translationY < 0) {
-
-
-                    //Log.d("TruckDetail", "trans:" + translationY + " offset:" + (mFlexibleSpaceHeight - mTabHeight));
-
                     mScrolled = true;
                     return true;
                 }
@@ -239,21 +193,6 @@ public class TruckDetailActivity extends BaseActivity implements ObservableScrol
         }
     };
 
-    private void showFab() {
-        if (!mFabIsShown) {
-            ViewPropertyAnimator.animate(mFab).cancel();
-            ViewPropertyAnimator.animate(mFab).scaleX(1).scaleY(1).setDuration(200).start();
-            mFabIsShown = true;
-        }
-    }
-
-    private void hideFab() {
-        if (mFabIsShown) {
-            ViewPropertyAnimator.animate(mFab).cancel();
-            ViewPropertyAnimator.animate(mFab).scaleX(0).scaleY(0).setDuration(200).start();
-            mFabIsShown = false;
-        }
-    }
 
     private Scrollable getCurrentScrollable() {
         Fragment fragment = getCurrentFragment();
@@ -280,28 +219,10 @@ public class TruckDetailActivity extends BaseActivity implements ObservableScrol
         float flexibleRange = mFlexibleSpaceHeight - getActionBarSize();
         ViewHelper.setAlpha(mOverlayView, ScrollUtils.getFloat(-translationY / flexibleRange, 0, 1));
 
-        /*
-        // Scale title text
-        float scale = 1 + ScrollUtils.getFloat((flexibleRange + translationY) / flexibleRange, 0, MAX_TEXT_SCALE_DELTA);
-        setPivotXToTitle();
-        ViewHelper.setPivotY(mFab, 0);
-        ViewHelper.setScaleX(mFab, scale);
-        ViewHelper.setScaleY(mFab, scale);*/
     }
 
     private Fragment getCurrentFragment() {
         return mPagerAdapter.getItemAt(mPager.getCurrentItem());
-    }
-
-    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
-    private void setPivotXToTitle() {
-        Configuration config = getResources().getConfiguration();
-        /*if (Build.VERSION_CODES.JELLY_BEAN_MR1 <= Build.VERSION.SDK_INT
-                && config.getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
-            ViewHelper.setPivotX(mFab, findViewById(android.R.id.content).getWidth());
-        } else {
-            ViewHelper.setPivotX(mFab, 0);
-        }*/
     }
 
     /**
@@ -310,7 +231,7 @@ public class TruckDetailActivity extends BaseActivity implements ObservableScrol
      */
     private class NavigationAdapter extends CacheFragmentStatePagerAdapter {
 
-        private final String[] TITLES = new String[]{"Om", "Status", "Omdöme", "Meny"};
+        private final String[] TITLES = new String[]{"Om", "Status", "Betyg", "Meny"};
 
         public NavigationAdapter(FragmentManager fm) {
             super(fm);
@@ -347,5 +268,21 @@ public class TruckDetailActivity extends BaseActivity implements ObservableScrol
         public CharSequence getPageTitle(int position) {
             return TITLES[position];
         }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
