2035d648d7073f5d30d9e88b6472be4f9a9e7128
Hugo Lindström <hugolm84@gmail.com>
2015-03-30 14:26:17 +0200

    onPause(): save received locations

--- app/src/main/java/pb/foodtruckfinder/Receiver/LocationReceiver.java
+++ app/src/main/java/pb/foodtruckfinder/Receiver/LocationReceiver.java
@@ -4,10 +4,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
+import android.os.Bundle;
 import android.util.Log;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import pb.foodtruckfinder.Service.GeofenceHandler;
 import pb.foodtruckfinder.Service.LocationHandler;
+import pb.foodtruckfinder.Service.LocationService;
 
 /**
  * Created by hugo on 26/03/15.
@@ -16,15 +20,40 @@ public class LocationReceiver extends BroadcastReceiver {
 
     private final static String TAG = "LocationReciever";
 
+    private ConcurrentLinkedQueue<Location> mOnPauseLocations = new ConcurrentLinkedQueue<>();
+    private ConcurrentLinkedQueue<Location> mOnPauseRemoteLocations = new ConcurrentLinkedQueue<>();
+
     private LocationReceiverCallback mCallback;
+    private boolean mIsPaused;
 
     public interface LocationReceiverCallback {
+        void onRemoteLocationReceived(final Location location, final String id);
         void onLocationReceived(final Location location);
         void onGeofenceReceived(final String geoString);
     }
 
     public void setReceiverCallback(LocationReceiverCallback callback) {
         mCallback = callback;
+    }
+
+    public void onPause() {
+        mIsPaused = true;
+    }
+
+    public void onResume() {
+        mIsPaused = false;
+    }
+
+    public Location getNextOnPausedLocation() {
+        if(mOnPauseLocations.isEmpty())
+            return null;
+        return mOnPauseLocations.remove();
+    }
+
+    public Location getNextOnPausedRemoteLocation() {
+        if(mOnPauseRemoteLocations.isEmpty())
+            return null;
+        return mOnPauseRemoteLocations.remove();
     }
 
     @Override
@@ -34,7 +63,12 @@ public class LocationReceiver extends BroadcastReceiver {
         if(intent.getAction().equals(LocationHandler.TAG)) {
             Location location = intent.getParcelableExtra(LocationHandler.LOC_PARCEL);
             if (location != null) {
-                mCallback.onLocationReceived(location);
+                if (mIsPaused) {
+                    Log.d(TAG, "Storing onPause location! Got:" + mOnPauseLocations.size());
+                    mOnPauseLocations.add(location);
+                } else {
+                    mCallback.onLocationReceived(location);
+                }
             }
         }
 
@@ -42,6 +76,24 @@ public class LocationReceiver extends BroadcastReceiver {
             final String geoString = intent.getStringExtra(GeofenceHandler.GEO_PARCEL);
             if(geoString != null && !geoString.isEmpty())
                 mCallback.onGeofenceReceived(geoString);
+        }
+
+        if(intent.getAction().equals(LocationService.TAG)) {
+            Location location = intent.getParcelableExtra(LocationService.REMOTE_LOC);
+            String id = intent.getStringExtra(LocationService.REMOTE_LOC_ID);
+            if (location != null) {
+
+                if(mIsPaused) {
+                    Log.d(TAG, "Storing onPause RemoteLocation! Got:" + mOnPauseRemoteLocations.size());
+                    Bundle locationBundle = new Bundle();
+                    locationBundle.putString(LocationService.REMOTE_LOC_ID, id);
+                    location.setExtras(locationBundle);
+                    mOnPauseRemoteLocations.add(location);
+                }
+                else {
+                    mCallback.onRemoteLocationReceived(location, id);
+                }
+            }
         }
     }
 }
